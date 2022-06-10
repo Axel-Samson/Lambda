a8aaafdfcda71875094b567cc4e9473b24619cce
Axel Samson <theo.auditore@gmail.com>
2022-06-10 15:41:30 -0300

    teste

--- src/Lambda/CalculoTeste2.java
+++ src/Lambda/CalculoTeste2.java
@@ -1,12 +1,28 @@
 package Lambda;
 
+import java.util.function.BinaryOperator;
+
 public class CalculoTeste2 {
     public static void main(String[] args) {
-        Calculo calculo = (x, y) -> {return x + y;};
-        System.out.println(calculo.executar(2, 3));
+        //interface BinaryOperator substitui a classe
+        //definindo o Calculo Lembrar-se de definir
+        //o tipo de parametro
+        //ex: double com 2.2 INTeger com 1
+
+        BinaryOperator<Double> calculo = (x, y) -> {
+            return x + y;
+        };
+        System.out.println(calculo.apply(2.0, 3.0));
+
+
+
+        BinaryOperator<Integer> calculo2 = (x, y) -> {
+            return x + y;
+        };
+        System.out.println(calculo2.apply(2, 3));
 
         calculo = (x, y) -> x * y;
-        System.out.println(calculo.executar(2, 3));
+        System.out.println(calculo2.apply((int) 3, (int) 3));
     }
 }
 
